efeae2fdaefa480d3b86c4b689626df5307ad0d8
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-05-09 22:03:18 +0300

    test

--- DH.WebUI/src/features/club-space-management/components/club-space-list/club-space-list.component.ts
+++ DH.WebUI/src/features/club-space-management/components/club-space-list/club-space-list.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-club-space-list',
   templateUrl: 'club-space-list.component.html',
   styleUrl: 'club-space-list.component.scss',
 })
-export class ClubSpaceListComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
+export class ClubSpaceListComponent {
 }
