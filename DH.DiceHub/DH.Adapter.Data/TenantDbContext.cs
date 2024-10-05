dd78830208805396e6d0e2346be62be0f784a8e3
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-10-05 10:35:32 +0300

    db

--- DH.DiceHub/DH.Adapter.Data/TenantDbContext.cs
+++ DH.DiceHub/DH.Adapter.Data/TenantDbContext.cs
@@ -31,6 +31,8 @@ public class TenantDbContext : DbContext, ITenantDbContext
 #endif
     }
 
+    #region games
+
     public DbSet<Game> Games { get; set; } = default!;
     public DbSet<GameReview> GameReviews { get; set; } = default!;
     public DbSet<GameLike> GameLikes { get; set; } = default!;
@@ -39,14 +41,40 @@ public class TenantDbContext : DbContext, ITenantDbContext
     public DbSet<GameReservation> GameReservations { get; set; } = default!;
     public DbSet<GameInventory> GameInventories { get; set; } = default!;
     public DbSet<GameQrCode> GameQrCodes { get; set; } = default!;
-    public DbSet<FailedJob> FailedJobs { get; set; } = default!;
+
+    #endregion games
+
+    #region events
+
     public DbSet<Event> Events { get; set; } = default!;
     public DbSet<EventImage> EventImages { get; set; } = default!;
     public DbSet<EventParticipant> EventParticipants { get; set; } = default!;
+
+    #endregion events
+
+    #region rooms
+
     public DbSet<Room> Rooms { get; set; } = default!;
     public DbSet<RoomParticipant> RoomParticipants { get; set; } = default!;
     public DbSet<RoomMessage> RoomMessages { get; set; } = default!;
     public DbSet<RoomInfoMessage> RoomInfoMessages { get; set; } = default!;
+
+    #endregion rooms
+
+    #region challenges
+
+    public DbSet<Challenge> Challenges { get; set; } = default!;
+    public DbSet<ChallengeStatistic> ChallengeStatistics { get; set; } = default!;
+    public DbSet<UserChallenge> UserChallenges { get; set; } = default!;
+    public DbSet<ChallengeReward> ChallengeRewards { get; set; } = default!;
+    public DbSet<ChallengeRewardImage> ChallengeRewardImages { get; set; } = default!;
+    public DbSet<UserChallengeReward> UserChallengeRewards { get; set; } = default!;
+    public DbSet<UserChallengePeriodPerformance> UserChallengePeriodPerformances { get; set; } = default!;
+
+    #endregion challenges
+
+    public DbSet<UserStatistic> UserStatistics { get; set; } = default!;
+    public DbSet<FailedJob> FailedJobs { get; set; } = default!;
 
     public T AcquireRepository<T>()
     {
