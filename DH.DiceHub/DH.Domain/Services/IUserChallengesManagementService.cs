a2fd45c1d2ee2afd8c6315e2e395d07cd85840b1
Mihael Pavlov <m.pavlov1405@abv.bg>
2024-10-08 12:33:28 +0300

    add service

---
+++ DH.DiceHub/DH.Domain/Services/IUserChallengesManagementService.cs
@@ -0,0 +1,8 @@
+﻿namespace DH.Domain.Services;
+
+public interface IUserChallengesManagementService
+{
+    Task InitiateNewUserChallenges(string userId, CancellationToken cancellationToken);
+
+    Task AddChallengeToUser(string userId, CancellationToken cancellationToken);
+}
