26a0615b8cf8ff3c28e735b2e5723d73286bdd28
Mihael Pavlov <m.pavlov1405@abv.bg>
2024-10-11 08:42:00 +0300

    Add QR Code Scan Audit

--- DH.DiceHub/DH.Adapter.QRManager/QRCodeContext.cs
+++ DH.DiceHub/DH.Adapter.QRManager/QRCodeContext.cs
@@ -1,19 +1,23 @@
-﻿using DH.Domain.Adapters.QRManager;
+﻿using DH.Domain.Adapters.Authentication;
+using DH.Domain.Adapters.QRManager;
 using DH.Domain.Adapters.QRManager.StateModels;
 using DH.Domain.Entities;
 using DH.Domain.Repositories;
+using System.Text.Json;
 
 namespace DH.Adapter.QRManager;
 
 public class QRCodeContext : IQRCodeContext
 {
-    private IQRCodeState _state;
-    private readonly IRepository<QrCodeScanAudit> scanAuditRepository;
+    IQRCodeState _state;
+    readonly IRepository<QrCodeScanAudit> scanAuditRepository;
+    readonly IUserContext userContext;
 
-    public QRCodeContext(IRepository<QrCodeScanAudit> scanAuditRepository)
+    public QRCodeContext(IRepository<QrCodeScanAudit> scanAuditRepository, IUserContext userContext)
     {
         _state = null!;
         this.scanAuditRepository = scanAuditRepository;
+        this.userContext = userContext;
     }
 
     public void SetState(IQRCodeState state)
@@ -30,7 +34,17 @@ public class QRCodeContext : IQRCodeContext
     {
         try
         {
+            var jsonData = JsonSerializer.Serialize(data);
+            var jsonErrorMessage = JsonSerializer.Serialize(errorMessage);
 
+            await this.scanAuditRepository.AddAsync(new QrCodeScanAudit
+            {
+                TraceId = traceId,
+                ScannedData = jsonData,
+                ErrorMessage = jsonErrorMessage,
+                ScannedAt = DateTime.UtcNow,
+                UserId = this.userContext.UserId
+            }, cancellationToken);
         }
         catch (Exception ex)
         {
--- DH.DiceHub/DH.Domain/Entities/QrCodeScanAudit.cs
+++ DH.DiceHub/DH.Domain/Entities/QrCodeScanAudit.cs
@@ -3,6 +3,7 @@
 public class QrCodeScanAudit
 {
     public int Id { get; set; }
+    public string TraceId { get; set; } = string.Empty;
     public string UserId { get; set; } = string.Empty;
     public string ScannedData { get; set; } = string.Empty;
     public string ErrorMessage { get; set; } = string.Empty;
