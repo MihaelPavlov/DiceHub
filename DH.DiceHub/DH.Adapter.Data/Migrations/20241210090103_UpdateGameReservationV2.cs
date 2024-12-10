9ca324032f3154fc4c22f54a88dee303fbf06db5
Mihael Pavlov <m.pavlov1405@abv.bg>
2024-12-10 21:34:51 +0200

    updating the game reservation model

---
+++ DH.DiceHub/DH.Adapter.Data/Migrations/20241210090103_UpdateGameReservationV2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DH.Adapter.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGameReservationV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PeopleCount",
+                table: "GameReservations",
+                newName: "NumberOfGuests");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "GameReservations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "GameReservations");
+
+            migrationBuilder.RenameColumn(
+                name: "NumberOfGuests",
+                table: "GameReservations",
+                newName: "PeopleCount");
+        }
+    }
+}
