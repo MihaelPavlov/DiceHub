a0ea35c4b7b98cd81a5b42deb6a8974067b28447
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-02-13 20:26:24 +0200

    add additional prop to challenge period

--- DH.DiceHub/DH.Domain/Models/ChallengeModels/Queries/GetUserChallengePeriodPerformanceQueryModel.cs
+++ DH.DiceHub/DH.Domain/Models/ChallengeModels/Queries/GetUserChallengePeriodPerformanceQueryModel.cs
@@ -1,7 +1,12 @@
-﻿namespace DH.Domain.Models.ChallengeModels.Queries;
+﻿using DH.Domain.Enums;
+
+namespace DH.Domain.Models.ChallengeModels.Queries;
 
 public class GetUserChallengePeriodPerformanceQueryModel
 {
     public int Id { get; set; }
     public int Points { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public TimePeriodType TimePeriodType { get; set; }
 }
--- DH.WebUI/src/entities/challenges/models/user-challenge-period-performance.model.ts
+++ DH.WebUI/src/entities/challenges/models/user-challenge-period-performance.model.ts
@@ -1,4 +1,9 @@
+import { TimePeriodType } from '../../common/enum/time-period-type.enum';
+
 export interface IUserChallengePeriodPerformance {
   id: number;
   points: number;
+  startDate: Date;
+  endDate: Date;
+  timePeriodType: TimePeriodType;
 }
