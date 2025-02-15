1257d5c1d2239bb55612e3527b89991cf747737e
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-02-15 08:07:39 +0200

    remove using

--- DH.DiceHub/DH.Adapter.ChallengesOrchestrator/SynchronizeUsersChallengesService.cs
+++ DH.DiceHub/DH.Adapter.ChallengesOrchestrator/SynchronizeUsersChallengesService.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace DH.Adapter.ChallengesOrchestrator;
 
