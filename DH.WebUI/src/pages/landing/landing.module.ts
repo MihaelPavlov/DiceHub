23b8d8e86d5422e0e01225ebfbfda59950fb0dbc
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-04-01 20:15:48 +0300

    make the dots always at the bottom

--- DH.WebUI/src/pages/landing/landing.module.ts
+++ DH.WebUI/src/pages/landing/landing.module.ts
@@ -3,10 +3,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { LandingRoutingModule } from './landing-routes.module';
 import { LandingComponent } from './page/landing.component';
 
-@NgModule({
-  declarations: [LandingComponent],
-  exports: [],
-  providers: [],
-  imports: [SharedModule, LandingRoutingModule],
-})
-export class LandingModule {}
+// @NgModule({
+//   declarations: [LandingComponent],
+//   exports: [],
+//   providers: [],
+//   imports: [SharedModule],
+// })
+// export class LandingModule {}
