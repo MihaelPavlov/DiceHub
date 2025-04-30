efc5c0fe18ca66385de706b33251556641754167
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-04-30 16:39:17 +0300

    Sending correct notification during event creation

--- DH.DiceHub/DH.Application/Events/Commands/CreateEventCommand.cs
+++ DH.DiceHub/DH.Application/Events/Commands/CreateEventCommand.cs
@@ -1,4 +1,9 @@
-﻿using DH.Domain.Entities;
+﻿using DH.Domain.Adapters.Authentication.Models;
+using DH.Domain.Adapters.Authentication.Models.Enums;
+using DH.Domain.Adapters.Authentication.Services;
+using DH.Domain.Adapters.PushNotifications;
+using DH.Domain.Adapters.PushNotifications.Messages;
+using DH.Domain.Entities;
 using DH.Domain.Models.EventModels.Command;
 using DH.Domain.Services;
 using DH.OperationResultCore.Exceptions;
@@ -7,15 +12,26 @@ using MediatR;
 
 namespace DH.Application.Events.Commands;
 
-public record CreateEventCommand(CreateEventModel Event, string? FileName, string? ContentType, MemoryStream? ImageStream) : IRequest<int>;
+public record CreateEventCommand(
+    CreateEventModel Event,
+    string? FileName,
+    string? ContentType,
+    MemoryStream? ImageStream) : IRequest<int>;
 
 internal class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, int>
 {
     readonly IEventService eventService;
+    readonly IUserService userService;
+    readonly IPushNotificationsService pushNotificationsService;
 
-    public CreateEventCommandHandler(IEventService eventService)
+    public CreateEventCommandHandler(
+        IEventService eventService,
+        IUserService userService,
+        IPushNotificationsService pushNotificationsService)
     {
         this.eventService = eventService;
+        this.userService = userService;
+        this.pushNotificationsService = pushNotificationsService;
     }
 
     public async Task<int> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -30,6 +46,16 @@ internal class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, i
             request.ImageStream,
             cancellationToken
         );
+
+        var users = await this.userService.GetUserListByRoles([Role.User, Role.Staff], cancellationToken);
+
+        await this.pushNotificationsService
+           .SendNotificationToUsersAsync(
+               users,
+               new NewEventAddedMessage(
+                request.Event.Name,
+                request.Event.StartDate.ToLocalTime()),
+               cancellationToken);
 
         return eventId;
     }
---
+++ DH.DiceHub/DH.Domain/Adapters/PushNotifications/Messages/NewEventAddedMessage.cs
@@ -0,0 +1,14 @@
+﻿using DH.Domain.Adapters.PushNotifications.Messages.Common;
+
+namespace DH.Domain.Adapters.PushNotifications.Messages;
+
+public class NewEventAddedMessage : MessageRequest
+{
+    public NewEventAddedMessage(string eventName, DateTime eventDate)
+    {
+
+        //TODO: Fix other notification dates
+        Title = "New Event was Added";
+        Body = $"New event {eventName} was created, for date - {eventDate.ToString("dd/MMM/yyyy HH:mm")}. Come and enjoy the journey with us";
+    }
+}
