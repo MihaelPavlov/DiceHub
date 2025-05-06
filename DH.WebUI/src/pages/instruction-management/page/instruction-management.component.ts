de631d20f93f151dfc7c3cb06bc11f6a66bdea9a
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-05-06 20:04:23 +0300

    imrpove instructions

--- DH.WebUI/src/pages/instruction-management/page/instruction-management.component.ts
+++ DH.WebUI/src/pages/instruction-management/page/instruction-management.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavigationService } from '../../../shared/services/navigation-service';
+import { AuthService } from '../../../entities/auth/auth.service';
 
 @Component({
   selector: 'app-instruction-management',
@@ -8,10 +9,6 @@ import { NavigationService } from '../../../shared/services/navigation-service';
   styleUrl: 'instruction-management.component.scss',
 })
 export class InstructionManagementComponent {
-  constructor(
-    private readonly router: Router,
-    private readonly navigationService: NavigationService
-  ) {}
   public defaultLinks = [
     { name: 'Reservations', path: '/instructions/reservation' },
     { name: 'Events', path: '/instructions/events' },
@@ -19,8 +16,22 @@ export class InstructionManagementComponent {
     { name: 'Meeples', path: '/instructions/meeples' },
   ];
 
+  constructor(
+    private readonly router: Router,
+    private readonly navigationService: NavigationService,
+    private readonly authService: AuthService
+  ) {}
+
+  public get isUserAuthenticated(): boolean {
+    return this.authService.getUser !== null;
+  }
+
   public navigateTo(path: string): void {
     this.navigationService.setPreviousUrl(this.router.url);
     this.router.navigateByUrl(path);
+  }
+
+  public backNavigateBtn() {
+    this.router.navigateByUrl('profile');
   }
 }
