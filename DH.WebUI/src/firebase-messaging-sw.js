0f79ea214d4c68a964c7b0bb5a86975de19b67d4
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-05-27 07:24:23 +0300

    Testing

--- DH.WebUI/src/firebase-messaging-sw.js
+++ DH.WebUI/src/firebase-messaging-sw.js
@@ -1,17 +1,17 @@
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.8.0/firebase-app-compat.js',
-);
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.8.0/firebase-messaging-compat.js',
-);
-  const app = firebase.initializeApp({
-    apiKey: "AIzaSyBSZ3ju-sFNOPAoLuw_q2PebmtQsQ3D13s",
-    authDomain: "dicehub-8c63f.firebaseapp.com",
-    projectId: "dicehub-8c63f",
-    storageBucket: "dicehub-8c63f.appspot.com",
-    messagingSenderId: "566204429306",
-    appId: "1:566204429306:web:cda989326f8fd750d02a43",
-    measurementId: "G-1SNNNLCVLW"
-  });
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/9.8.0/firebase-app-compat.js',
+// );
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/9.8.0/firebase-messaging-compat.js',
+// );
+//   const app = firebase.initializeApp({
+//     apiKey: "AIzaSyBSZ3ju-sFNOPAoLuw_q2PebmtQsQ3D13s",
+//     authDomain: "dicehub-8c63f.firebaseapp.com",
+//     projectId: "dicehub-8c63f",
+//     storageBucket: "dicehub-8c63f.appspot.com",
+//     messagingSenderId: "566204429306",
+//     appId: "1:566204429306:web:cda989326f8fd750d02a43",
+//     measurementId: "G-1SNNNLCVLW"
+//   });
   
-  const messaging = firebase.messaging(app);
+//   const messaging = firebase.messaging(app);
