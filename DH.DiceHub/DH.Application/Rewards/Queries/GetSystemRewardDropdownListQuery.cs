9ff97aae836c88de8a7df5ab9a2a5eabbae53784
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-06-29 19:57:16 +0300

    system reward dropdown list

---
+++ DH.DiceHub/DH.Application/Rewards/Queries/GetSystemRewardDropdownListQuery.cs
@@ -0,0 +1,18 @@
+﻿using DH.Domain.Entities;
+using DH.Domain.Models.RewardModels.Queries;
+using DH.Domain.Repositories;
+using MediatR;
+
+namespace DH.Application.Rewards.Queries;
+
+public record GetSystemRewardDropdownListQuery : IRequest<List<GetSystemRewardDropdownListQueryModel>>;
+
+internal class GetSystemRewardDropdownListQueryHandler(IRepository<ChallengeReward> repository) : IRequestHandler<GetSystemRewardDropdownListQuery, List<GetSystemRewardDropdownListQueryModel>>
+{
+    readonly IRepository<ChallengeReward> repository = repository;
+
+    public async Task<List<GetSystemRewardDropdownListQueryModel>> Handle(GetSystemRewardDropdownListQuery request, CancellationToken cancellationToken)
+    {
+        return await this.repository.GetWithPropertiesAsync<GetSystemRewardDropdownListQueryModel>(x => !x.IsDeleted, x => new(x.Id, x.Name), cancellationToken);
+    }
+}
