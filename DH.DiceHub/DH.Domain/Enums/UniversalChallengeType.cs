e1e4049a8c5bea56be8bd856e353bff909bafa4a
Mihael Pavlov <m.pavlov1405@abv.bg>
2025-10-12 20:10:09 +0300

    first version universal challenge types

---
+++ DH.DiceHub/DH.Domain/Enums/UniversalChallengeType.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace DH.Domain.Enums;
+
+public enum UniversalChallengeType
+{
+    [EnumMember(Value = "Play Games")]
+    PlayGames,
+
+    [EnumMember(Value = "Join Meeple Rooms")]
+    JoinMeepleRooms,
+
+    [EnumMember(Value = "Join Events")]
+    JoinEvents,
+
+    [EnumMember(Value = "Use Rewards")]
+    UseRewards,
+
+    [EnumMember(Value = "Rewards Granted")]
+    RewardsGranted,
+
+    [EnumMember(Value = "Buy Items")]
+    BuyItems,
+
+    [EnumMember(Value = "Play Favorite Game")]
+    PlayFavoriteGame,
+
+    [EnumMember(Value = "Top 3 Challenge Leaderboard")]
+    Top3ChallengeLeaderboard,
+
+    [EnumMember(Value = "Top 3 Streak Leaderboard")]
+    Top3StreakLeaderboard
+}
